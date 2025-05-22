e2f649a1214f1bba98c60fbb93a86d4a1973c8d0
anchit mandal <21f1000692@ds.study.iitm.ac.in>
2025-05-22 22:06:44 +0530

    new file:   day3/backend/app.py 	new file:   day3/backend/config_file.py 	new file:   day3/backend/init_db.py 	new file:   day3/backend/models.py 	new file:   day3/backend/requirements.txt 	new file:   day3/backend/routes/category.py 	new file:   day3/backend/routes/product.py 	new file:   day3/backend/routes/security.py 	new file:   day3/frontend/.gitignore 	new file:   day3/frontend/README.md 	new file:   day3/frontend/babel.config.js 	new file:   day3/frontend/jsconfig.json 	new file:   day3/frontend/package-lock.json 	new file:   day3/frontend/package.json 	new file:   day3/frontend/public/favicon.ico 	new file:   day3/frontend/public/img/icons/android-chrome-192x192.png 	new file:   day3/frontend/public/img/icons/android-chrome-512x512.png 	new file:   day3/frontend/public/img/icons/android-chrome-maskable-192x192.png 	new file:   day3/frontend/public/img/icons/android-chrome-maskable-512x512.png 	new file:   day3/frontend/public/img/icons/apple-touch-icon-120x120.png 	new file:   day3/frontend/public/img/icons/apple-touch-icon-152x152.png 	new file:   day3/frontend/public/img/icons/apple-touch-icon-180x180.png 	new file:   day3/frontend/public/img/icons/apple-touch-icon-60x60.png 	new file:   day3/frontend/public/img/icons/apple-touch-icon-76x76.png 	new file:   day3/frontend/public/img/icons/apple-touch-icon.png 	new file:   day3/frontend/public/img/icons/favicon-16x16.png 	new file:   day3/frontend/public/img/icons/favicon-32x32.png 	new file:   day3/frontend/public/img/icons/msapplication-icon-144x144.png 	new file:   day3/frontend/public/img/icons/mstile-150x150.png 	new file:   day3/frontend/public/img/icons/safari-pinned-tab.svg 	new file:   day3/frontend/public/index.html 	new file:   day3/frontend/public/robots.txt 	new file:   day3/frontend/src/App.vue 	new file:   day3/frontend/src/assets/logo.png 	new file:   day3/frontend/src/components/HelloWorld.vue 	new file:   day3/frontend/src/main.js 	new file:   day3/frontend/src/registerServiceWorker.js 	new file:   day3/frontend/src/router/index.js 	new file:   day3/frontend/src/store/index.js 	new file:   day3/frontend/src/views/AboutView.vue 	new file:   day3/frontend/src/views/HomeView.vue 	new file:   day3/frontend/vue.config.js 	new file:   day3/vue-project/.gitattributes 	new file:   day3/vue-project/.gitignore 	new file:   day3/vue-project/.prettierrc.json 	new file:   day3/vue-project/.vscode/extensions.json 	new file:   day3/vue-project/README.md 	new file:   day3/vue-project/index.html 	new file:   day3/vue-project/jsconfig.json 	new file:   day3/vue-project/package-lock.json 	new file:   day3/vue-project/package.json 	new file:   day3/vue-project/public/favicon.ico 	new file:   day3/vue-project/src/App.vue 	new file:   day3/vue-project/src/assets/base.css 	new file:   day3/vue-project/src/assets/logo.svg 	new file:   day3/vue-project/src/assets/main.css 	new file:   day3/vue-project/src/components/HelloWorld.vue 	new file:   day3/vue-project/src/components/TheWelcome.vue 	new file:   day3/vue-project/src/components/WelcomeItem.vue 	new file:   day3/vue-project/src/components/icons/IconCommunity.vue 	new file:   day3/vue-project/src/components/icons/IconDocumentation.vue 	new file:   day3/vue-project/src/components/icons/IconEcosystem.vue 	new file:   day3/vue-project/src/components/icons/IconSupport.vue 	new file:   day3/vue-project/src/components/icons/IconTooling.vue 	new file:   day3/vue-project/src/main.js 	new file:   day3/vue-project/src/router/index.js 	new file:   day3/vue-project/src/views/AboutView.vue 	new file:   day3/vue-project/src/views/HomeView.vue 	new file:   day3/vue-project/vite.config.js

---
+++ day3/backend/app.py
@@ -0,0 +1,164 @@
+from flask import Flask, request
+from flask_security import Security, auth_required, roles_accepted
+from flask_restful import Api
+from models import db, user_datastore
+
+
+def create_app():
+
+    init_app = Flask(__name__)
+
+    from config_file import localDevConfig
+    init_app.config.from_object(localDevConfig)
+
+    db.init_app(init_app)
+
+    Security(init_app, user_datastore)
+
+    init_api = Api(init_app, prefix='/api')
+
+    return init_app, init_api
+
+app, api = create_app()
+
+from routes.category import getCategory, updateCategory
+api.add_resource(getCategory, '/category') #localhost:5000/api/category
+api.add_resource(updateCategory, '/category/update')
+
+
+@app.route('/')
+def index():
+    return {"message": "Hello, World!"}
+
+
+@app.route('/register', methods=['POST'])
+def register():
+    data = request.get_json()
+    email = data.get('email')
+    password = data.get('password')
+    username = data.get('username')
+    if not password:
+        return {"error": "Password is required"}, 400
+    
+    if not email and not username:
+        return {"error": "Email and username is required"}, 400
+    
+    if user_datastore.find_user(email=email) or user_datastore.find_user(username=username):
+        return {"error": "User already exists"}, 400
+    
+    user_datastore.create_user(email=email, password=password, username=username, roles=["user"])
+    db.session.commit()
+
+    return {"message": "User registered successfully"}, 201
+
+
+@app.route('/login', methods=['POST'])
+def login():
+    data = request.get_json()
+    email = data.get('email')
+    username = data.get('username')
+    password = data.get('password')
+
+    if not email and not username:
+        return {"error": "Email or username is required"}, 400
+
+    if not password:
+        return {"error": "Password is required"}, 400
+
+    user = user_datastore.find_user(email=email) if email else user_datastore.find_user(username=username)
+    if not user:
+        return {"error": "User not found, register with us"}, 404
+
+    if user.password != password:
+        return {"error": "Invalid password"}, 401
+    
+    if not user.is_active:
+        return {"error": "User is inactive, contact admin"}, 403
+
+    return {
+        "message": "Login successful",
+        "authToken": user.get_auth_token(),
+        "user": {"email": user.email, "username": user.username, "roles": [role.name for role in user.roles]}
+        }, 200
+
+
+@app.route('/test', methods=['POST'])
+@auth_required('token')
+@roles_accepted('admin')
+def test():
+    return {"message": "Test route running successfully"}
+
+@app.route('/category', methods=['GET', 'POST']) #localhost:5000/category
+@auth_required('token')
+def get_category():
+    if request.method == 'POST':
+        from models import Category
+        data = Category.get_all()
+        if not data:
+            return {
+                "error": "No categories found"
+            }, 404
+        return {
+            "categories": data
+        }, 200
+    
+    if request.method == 'GET':
+        id = request.json.get('id')
+        if not id:
+            return {
+                "error": "ID is required"
+            }, 400
+        from models import Category
+        data = Category.get_by_id(id)
+        if not data:
+            return {
+                "error": "Category not found"
+            }, 404
+        return {
+            "category": data
+        }, 200  
+
+@app.route('/category/update', methods=['PUT', 'DELETE', 'POST'])
+@auth_required('token')
+@roles_accepted('admin')
+def update_category():
+    from models import Category
+    data = request.json
+    if request.method == 'POST':
+        if Category.create(data.get('name'), data.get('description')):
+            return {
+                "message": "Category created successfully"
+            }, 201
+        else:
+            return {
+                "error": "Category already exists"
+            }, 400
+
+    if request.method == 'PUT':
+        id = data.get('id')
+        if Category.update(id, data.get('name'), data.get('description')):
+            return {
+                "message": "Category updated successfully"
+            }, 200
+        else:
+            return {
+                "error": "Category not found"
+            }, 404
+
+    elif request.method == 'DELETE':
+        if Category.delete(data.get('id')) == 3:
+            return {
+                "message": "Category deleted successfully"
+            }, 201
+        elif Category.delete(data.get('id')) == 2:
+            return {
+                "message": "Category restored successfully"
+            }, 201
+        else:
+            return {
+                "error": "Category not found"
+            }, 404
+
+
+if __name__ == '__main__':
+    app.run()
